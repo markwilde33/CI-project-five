0b63019c9df8494262e35880e1156e0e5499b229
Mark Wilde <markwilde29@gmail.com>
2019-04-07 10:26:36 +0000

    Updated

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME', 'interstellar-issue-tracker.herokuapp.com')]
-
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'interstellar-issue-tracker.herokuapp.com']
+                                              
 
 # Application definition
 
