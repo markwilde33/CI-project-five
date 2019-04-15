91c95c128c6913bcecfecffb926a57aec8fcf88a
Mark Wilde <markwilde29@gmail.com>
2019-04-15 14:45:49 +0000

    Updated password reset settings and pages. Users can enter their name or email address to request a new password.

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -152,3 +152,9 @@ STRIPE_SECRET = os.environ.get('STRIPE_SECRET')
 
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
